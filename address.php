cc11b48d0dc02dbd64d92db6c900dd3d670ed38f
murakamikayo <k-mura@asojuku.ac.jp>
2021-11-15 12:24:20 +0900

    2021/11/15更新

--- address.php
+++ address.php
@@ -1,35 +1,45 @@
 <?php
 
-$url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060&collback=test";
+$url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=8120016&collback=test";
 $json = file_get_contents($url);
 
-$data = json_decode($json);
+//true⇒連想配列／false（なし）⇒オブジェクト型
+$data = json_decode($json,true);
 
-$address1 = $data->results[0]->address1;
-$address2 = $data->results[0]->address2;
-$address3 = $data->results[0]->address3;
-$kana1 = $data->results[0]->kana1;
-$kana2 = $data->results[0]->kana2;
-$kana3 = $data->results[0]->kana3;
+$address1 = $data['results'][0]['address1'];
+$address2 = $data['results'][0]['address2'];
+$address3 = $data['results'][0]['address3'];
 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="utf-8">
-    <title>住所</title>
     <link rel="stylesheet" href="css/style.css">
-    <input type="text" name="address" value="">
-    <a href="address.php"> <button>住所検索</button></a><br>
-    <input type="text" value="<?=$address1 ?>"><br>
-    <input type="text" value="<?=$address2 ?>"><br>
-    <input type="text" value="<?=$address3 ?>"><br>
-    <input type="text" value="<?=$kana1 ?>"><br>
-    <input type="text" value="<?=$kana2 ?>"><br>
-    <input type="text" value="<?=$kana3 ?>"><br>
-
 </head>
 <body>
-
+<h2>住所検索</h2>
+<title>住所</title>
+<form action="" method="POST">
+<dl>
+    <dt>郵便番号</dt>
+    <dd><input type="text" name="address" value="">
+        <button onclick="address()">住所検索</button></dd>
+    <dt>住所1</dt>
+    <dd><input type="text" value="<?=$address1 ?>"></dd>
+    <dt>住所2</dt>
+    <dd><input type="text" value="<?=$address2 ?>"></dd>
+    <dt>住所3</dt>
+    <dd><input type="text" value="<?=$address3 ?>"></dd>
+    <dt>番地、マンション名など</dt>
+    <dd><input type="text" value=""></dd>
+</dl>
+<button type="submit">送信</button>
+</form>
+<script>
+    function address(){
+        alert("address");
+    }
+</script>
 </body>
 </html>
